c25ced431c7a339df9d91d73725a44df92f366f7
czernous-devexpress <anton.chernous@devexpress.com>
2021-02-22 13:01:33 +0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    roots: ['<rootDir>/src'],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    verbose: true,
+    testPathIgnorePatterns: ['/node_modules/'],
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        },
+    },
+};
